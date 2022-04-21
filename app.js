858ee73fd638614a887ca9a65d6a950d24815a7e
Bruno-Martin Schuster <bruno_schuster@gmx.de>
2022-04-21 22:07:25 +0200

    Add locig for different operators and equal sign Also +/-  sign and point for float numbers.  Operator chain missing

--- app.js
+++ app.js
@@ -51,32 +51,71 @@ const gF_operate = (num1, num2, operator) => {
     }
 }
 
+const gF_clearCalc = () => {
+
+
+    gV_firstOperand = '';
+    gV_secondOperand = '';
+    gV_operatorCounter = 0;
+    gV_numberBuffer = '';
+}
+
 const gE_CalcContainer = document.querySelector('#calc-container');
-const gE_calcDisplay = document.querySelector('#display');
+let gE_calcDisplay = document.querySelector('#display');
 let gV_numberBuffer = '';
 let gV_firstOperand = '';
 let gV_secondOperand = '';
 let gV_operator = '';
 let gV_operatorCounter = 0;
+let targetBtn = null;
+let gV_solution = '';
 
 gE_CalcContainer.addEventListener('click', (event) => {
 
-    if (event.target.tagName === 'BUTTON') {
+    event.preventDefault();
 
-        if (event.target.className === 'number') {
-            gE_calcDisplay.textContent += event.target.textContent;
-            gV_numberBuffer += event.target.textContent;
-            console.log(gV_numberBuffer);
-        }else if (event.target.className === 'operator') {
-            console.log('operator');
+    targetBtn = event.target;
+
+    if (targetBtn.tagName === 'BUTTON') {
+
+        if (targetBtn.id === 'equals' || gV_operatorCounter === 2) {
+
+            console.log(gV_operatorCounter);
+            gV_secondOperand = gV_numberBuffer;
+            gV_solution = gF_operate(gV_firstOperand, gV_secondOperand, gV_operator);
+            gE_calcDisplay.textContent = gV_solution;
+            gF_clearCalc();
+            gV_numberBuffer = gV_solution;
+
+        } else if (targetBtn.className === 'operator') {
+
+            gV_operator = targetBtn.textContent;
             gV_operatorCounter++;
             gV_firstOperand = gV_numberBuffer;
-        }else if (event.target.className.includes('sign')){
+            gV_numberBuffer = '';
+            gE_calcDisplay.textContent = '';
+
+        } else if (targetBtn.className.includes('sign')) {
+
             console.log('sign');
-        }else if (event.target.id === 'clear-calc'){
-            console.log('clear');
-        }else if (event.target.id === 'equals'){
-            console.log('equals');
+            if (targetBtn.id === 'point') {
+                gE_calcDisplay.textContent += targetBtn.textContent;
+                gV_numberBuffer += targetBtn.textContent;
+            } else if (targetBtn.id === 'pre-sign') {
+                gE_calcDisplay.textContent = String((+gE_calcDisplay.textContent) * (-1));
+                gV_numberBuffer = String((+gV_numberBuffer) * (-1));
+            }
+
+        } else if (targetBtn.id === 'clear-calc') {
+
+            gF_clearCalc();
+            gE_calcDisplay.textContent = '';
+
+        } else if (targetBtn.className === 'number') {
+
+
+            gE_calcDisplay.textContent += targetBtn.textContent;
+            gV_numberBuffer += targetBtn.textContent;
         }
     }
 
